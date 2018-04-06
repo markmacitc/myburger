e892ccc6cd6b935f37f30cb6eb1091539ecc301b
Mark McDonald <longryder@gmail.com>
2018-04-06 16:16:35 +0100

    First display of burger

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Aux from '../../hoc/Aux';
+import classes from './Layout.css';
+
+
+const layout = (props) => (
+    <Aux>
+    <div>Toolbar, Sidebar, Backdrop</div>
+    <main class={classes.Content}>
+        {props.children}
+    </main>    
+    </Aux>
+);
+
+export default layout;
