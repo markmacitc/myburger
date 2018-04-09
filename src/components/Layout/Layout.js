9536073b6ef61197883fb5d34076179278710908
Mark McDonald <longryder@gmail.com>
2018-04-09 16:38:07 +0100

    Burger builder  main functionality complete

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,7 +7,7 @@ import classes from './Layout.css';
 const layout = (props) => (
     <Aux>
     <div>Toolbar, Sidebar, Backdrop</div>
-    <main class={classes.Content}>
+    <main className={classes.Content}>
         {props.children}
     </main>    
     </Aux>
