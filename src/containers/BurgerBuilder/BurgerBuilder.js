f84ad57e376e205ffaa703d5d99b1cf7fe8edcbc
Mark McDonald <longryder@gmail.com>
2018-04-28 11:38:14 +0100

    End of day commit

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -25,7 +25,8 @@ class BurgerBuilder extends Component{
         loading: false,
         error:false
     }
-    componentWillMount() {
+    componentDidMount() {
+        console.log(this.props);
         axios.get('https://react-my-burger-39bac.firebaseio.com/ingredients.json')
         .then(response => {
             this.setState({ingredients: response.data});
@@ -87,30 +88,39 @@ class BurgerBuilder extends Component{
 
     purchaseContinueHandler =() => {
        // alert('You Continue');
-       this.setState({loading:true});
-       const order = {
-           ingredients: this.state.ingredients,
-           price: this.state.totalPrice,
-           customer: {
-            name: 'Mark McDonald',
-            address: {
-                street: 'High Street',
-                postCode: 'PL1 7PN',
-                city: 'Plymouth',
-                county: 'Devon',
-                country: 'United Kingdom'
-            },
-            email: 'test@test.com'
-           },
-           deliveryMethod: 'Carrier pigeon'
-       }
-        axios.post('/orders.json',order)
-        .then(response => {
-            this.setState({loading:false,purchasing:false});  
-        })
-        .catch(error => {
-            this.setState({loading:false,purchasing:false});   
-        })
+    //    this.setState({loading:true});
+    //    const order = {
+    //        ingredients: this.state.ingredients,
+    //        price: this.state.totalPrice,
+    //        customer: {
+    //         name: 'Mark McDonald',
+    //         address: {
+    //             street: 'High Street',
+    //             postCode: 'PL1 7PN',
+    //             city: 'Plymouth',
+    //             county: 'Devon',
+    //             country: 'United Kingdom'
+    //         },
+    //         email: 'test@test.com'
+    //        },
+    //        deliveryMethod: 'Carrier pigeon'
+    //    }
+    //     axios.post('/orders.json',order)
+    //     .then(response => {
+    //         this.setState({loading:false,purchasing:false});  
+    //     })
+    //     .catch(error => {
+    //         this.setState({loading:false,purchasing:false});   
+    //     })
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+        queryParams.push(encodeURIComponent(i) + "=" + encodeURIComponent(this.state.ingredients[i]));
+    };
+    const queryString = queryParams.join('&');
+    this.props.history.push({
+        pathname: '/checkout',
+        search: '?' + queryString
+    });
     }
 
 
